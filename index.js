a9ccf3ed4916e76684d1af48e205bc449292a01b
Venkatesh <inventvenkat@gmail.com>
2016-03-04 16:01:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,4 @@ http.createServer(function (request, response) {
 
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end('venkatesh\nSathish');
-	console.log("hi");
 }).listen(8082);
-
-console.log('Server running at http://127.0.0.1:8082/');
